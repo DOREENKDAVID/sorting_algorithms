36229606c3d5744a4ee5da9eb6ccc50b8e078373
DOREENDAVID <doreenkinya153@gmail.com>
2023-06-26 10:51:02 -0700

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -19,7 +19,9 @@ void shell_sort(int *array, size_t size)
 	int temp;
 	size_t gap = 1;
 
-	for (gap = (gap * 3) + 1; gap > 0; gap = gap / 3)
+	while (gap <= size / 3)
+		gap = (gap * 3) + 1;
+	while (gap > 0)
 	{
 		for (j = gap; j < size; j++)
 		{
@@ -34,6 +36,7 @@ void shell_sort(int *array, size_t size)
 		}
 		array[i] = temp;
 		}
+	gap = (gap - 1) / 3;
 	print_array(array, size);
 
 	}
